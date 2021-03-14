4f4f9a724c6f37a21aee01006982aed77fdeb0a6
Azhar Rahi <azhar.rahi@carlicence.co.za>
2021-03-14 15:55:36 +0100

    First Commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Sum(double x, double y)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Calculator calculator = new Calculator();
+            double sum = calculator.Sum(3, 5);
+            Assert.AreEqual<double>(8, sum);
+        }
+    }
+}
